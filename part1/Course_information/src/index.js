5296b4d6c8e5986d7e4bddc56693a65a96b099d4
Heidi <info@risako.fi>
2020-09-16 19:34:06 +0300

    something

--- part1/Course_information/src/index.js
+++ part1/Course_information/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header =(props) => {
+  console.log(props)
   return (
     <div>
       <h1> {props.course} </h1>
@@ -11,9 +12,18 @@ const Header =(props) => {
 const Content =(props) => {
   return(
     <div>
-      <p>{props.part1} {props.exercises1} </p>
+      <Part part1={props.part1.name} exercises={props.pare1.exercises} />
       <p>{props.part2} {props.exercises2} </p>
       <p>{props.part3} {props.exercises3} </p>
+    </div>
+  )
+}
+const Part =(props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
     </div>
   )
 }
@@ -29,7 +39,7 @@ const App = () => {
       const course = 'Half Stack application development'
       const part1 = 'Fundamentals of React'
       const exercises1 = 10 
-      const part2 = 'Using props to pass data2' 
+      const part2 = 'Using props to pass data' 
       const exercises2 = 7
       const part3 = 'State of a component' 
       const exercises3 = 14
