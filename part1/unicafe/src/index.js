4926f91ff6944fbad2202764168623b4f796b504
Heidi Keskitalo SOFTK20P <heidi.keskitalo@centria.fi>
2020-09-18 14:49:01 +0300

    Ex.1.7 Done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+
 const App = (props) => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const increaseByOne1 = () => setGood(good +1)
   const [neutral, setNeutral] = useState(0)
@@ -10,16 +11,15 @@ const App = (props) => {
   const [bad, setBad] = useState(0)
   const increaseByone3 = () => setBad(bad +1)
 
-
   const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>
       {text}
     </button>
   )
-  
+
   return (
     <div>
-      <h1>give feedback</h1>
+       <h1>give feedback</h1>
        <Button
         handleClick={increaseByOne1}
         text='good'
@@ -33,18 +33,17 @@ const App = (props) => {
         text='bad'
       />    
       <h2>statistics</h2>   
-          
       <p>good  {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
       <p>avarage {(good - bad )/(good + neutral + bad)}</p>
       <p>positive {good/(good + neutral + bad) * 100}</p>
-            
+      
     </div>
   )
 }
-//avarage yläpuolella ei toimi.
+
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
