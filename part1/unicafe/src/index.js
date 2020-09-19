d69e0f9f5b3c096a024acb8b4747f0e10c074949
Heidi <info@risako.fi>
2020-09-19 15:34:52 +0300

    EX.1.9.Done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
+const Statisticks = (props) => {
+  return (
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
+}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text} 
+  </button>
+)
 
 const App = (props) => {
   const [good, setGood] = useState(0)
@@ -11,34 +22,22 @@ const App = (props) => {
   const [bad, setBad] = useState(0)
   const increaseByone3 = () => setBad(bad +1)
 
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
+ 
+
 
   return (
     <div>
-       <h1>give feedback</h1>
-       <Button
-        handleClick={increaseByOne1}
-        text='good'
-      />
-      <Button
-        handleClick={increaseByOne2}
-        text='neutral'
-      />     
-      <Button
-        handleClick={increaseByone3}
-        text='bad'
-      />    
+      <h1>give feedback</h1>
+      <Button handleClick={increaseByOne1} text='good' />
+      <Button handleClick={increaseByOne2} text='neutral' />     
+      <Button handleClick={increaseByone3} text='bad' /> 
       <h2>statistics</h2>   
-      <p>good  {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>avarage {(good - bad )/(good + neutral + bad)}</p>
-      <p>positive {good/(good + neutral + bad) * 100}</p>
+      <Statisticks text='good' value={good} />
+      <Statisticks text='neutral' value={neutral} />
+      <Statisticks text= 'bad' value={bad} />
+      <Statisticks text='all' value={good + neutral + bad} />
+      <Statisticks text='avarage' value={(good - bad )/(good + neutral + bad)}/>
+      <Statisticks text='positive' value={good/(good + neutral + bad) * 100}/>  
       
     </div>
   )
