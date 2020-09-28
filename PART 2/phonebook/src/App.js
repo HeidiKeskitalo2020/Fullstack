c366bfc847cea388354b614e5c0d722ca8b32685
Heidi <info@risako.fi>
2020-09-28 14:53:02 +0300

    EX.2.8 DONE

--- PART 2/phonebook/src/App.js
+++ PART 2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   
   const addPerson = (event) => {
@@ -12,6 +13,7 @@ const App = () => {
     console.log('button clicked', event.target)
     const personObject = {
       name: newName,
+      number: newNumber,
       date: new Date().toISOString(),
       id: persons.length + 1,
     
@@ -25,13 +27,18 @@ const App = () => {
       {
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
     }
   }
+  
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
   
   return (
     <div>
@@ -42,13 +49,16 @@ const App = () => {
             onChange={handlePersonChange}/>
         </div>
         <div>
-            
+            number: <input value={newNumber}
+            onChange={handleNumberChange} />
+        </div>
+        <div>
             <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person => 
-          <div key={person.name}> {person.name} 
+          <div key={person.name}> {person.name} {person.number}
           </div>
           )}
     </div>
