381693047e3e0b4ac8777610d1c1e5baab94cfd8
Heidi <info@risako.fi>
2020-09-28 13:19:44 +0300

    EX.2.6 Done

--- PART 2/phonebook/src/App.js
+++ PART 2/phonebook/src/App.js
@@ -10,8 +10,20 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
-  }
+    const personObject = {
+      name: newName,
+      date: new Date().toISOString(),
+      id: persons.length + 1,
+    
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
   
+  }
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   
   return (
@@ -19,21 +31,21 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-            name: <input />
+            name: <input value={newName} 
+            onChange={handlePersonChange}/>
         </div>
         <div>
+            
             <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-     
-          {persons.map(person => 
+        {persons.map(person => 
           <div key={person.name}> {person.name} 
           </div>
-        )}
+          )}
     </div>
   )
-
 }
 
 export default App
