04af3f5b82ca46babb0168de6644981a84f6a26c
Heidi <info@risako.fi>
2020-10-13 10:21:44 +0300

    Edit

--- PART 2/phonebook/src/index.js
+++ PART 2/phonebook/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import App from'./App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/persons')
+/*const promise = axios.get('http://localhost:3001/persons')
 promise.then(response => { 
 console.log(response)
-})
+})*/
 
 /*const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)*/
