77f1c534dd0c640e5d3f3f1c26150c92f4941a63
Heidi <info@risako.fi>
2020-10-30 15:31:54 +0200

    EX. 4.8. Done

---
+++ PART4/Blog/test/blog_api.test.js
@@ -0,0 +1,44 @@
+const { response } = require('express')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      content: 'HTML is easy',
+      date: new Date(),
+      important: false,
+    },
+    {
+      content: 'Browser can execute only Javascript',
+      date: new Date(),
+      important: true,
+    },
+  ]
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+test('Blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('There are correct amount of blogs returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
