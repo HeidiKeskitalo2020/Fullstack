431f5a7052ca03f60169e651912adec21e493abf
Heidi <info@risako.fi>
2020-11-09 15:20:39 +0200

    Ex. 5.2 Done

--- PART5/bloglist-frontend/src/App.js
+++ PART5/bloglist-frontend/src/App.js
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async(event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -22,6 +30,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -63,7 +76,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in
+      <button onClick={() => {
+        window.localStorage.removeItem('loggedNoteappUser')
+        setUser(null)
+      }}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
