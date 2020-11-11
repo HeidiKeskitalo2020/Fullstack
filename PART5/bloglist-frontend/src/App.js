059fe3afc6f70f3d610db0442d5d179a10d9d470
Heidi <info@risako.fi>
2020-11-11 11:48:47 +0200

    EX.5.9 Done

--- PART5/bloglist-frontend/src/App.js
+++ PART5/bloglist-frontend/src/App.js
@@ -88,6 +88,11 @@ const App = () => {
       }, 3000)  
     }
   }
+
+  const sortBlogs = (blogs) => {
+    return blogs.sort((a,b) => b.likes - a.likes)
+  }
+
   if(user === null) {
     return (
       <div>
@@ -119,7 +124,7 @@ const App = () => {
       </Toggleable>
 
       </div>
-      {blogs.map(blog =>
+      {sortBlogs(blogs).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
       )}
     </div>
