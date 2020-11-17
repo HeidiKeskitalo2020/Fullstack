7950c73c8ca3df4a89b8e9d328d8f0781f6d6f47
Heidi <info@risako.fi>
2020-11-17 13:52:07 +0200

    Ex. 6.10

--- PART6/redux-anecdotes/src/App.js
+++ PART6/redux-anecdotes/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import AnecdoteForm from './components/anecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <AnecdoteForm />
       <AnecdoteList />
     </div>
---
+++ PART6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+const notificationReducer = (state = 'Elementary message', action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.notification
+    default:
+      return state
+  }
+}
+
+
+
+export default notificationReducer
